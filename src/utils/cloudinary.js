0a2de124976c102d0f3fa4a05c0db103b1140780
Gobinda Das <dasgobinda9955@gmail.com>
2025-06-21 23:30:55 +0530

    feat: add Cloudinary configuration with support for uploading and deleting images and videos

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,50 @@
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+
+
+dotenv.config({});
+
+cloudinary.config({
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+});
+
+
+
+export const uploadMedia = async (file) => {
+  try {
+
+    const uploadResponse = await cloudinary.uploader.upload(file, {
+      resource_type: "auto",
+    });
+
+
+    return uploadResponse;
+
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+
+export const deleteMediaFromCloudinary = async (publicId) => {
+  try {
+
+    await cloudinary.uploader.destroy(publicId);
+
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+
+export const deleteVideoFromCloudinary = async (publicId) => {
+  try {
+
+    await cloudinary.uploader.destroy(publicId, { resource_type: "video" });
+
+  } catch (error) {
+    console.error(error);
+  }
+}
