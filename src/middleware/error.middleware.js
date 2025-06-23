63fd4b93ce076b43267cb529850baf20bd72e826
Gobinda Das <dasgobinda9955@gmail.com>
2025-06-23 11:10:16 +0530

    feat: add AppError class for standardized error handling

--- src/middleware/error.middleware.js
+++ src/middleware/error.middleware.js
@@ -1,3 +1,16 @@
+// Custom error class
+export class AppError extends Error {
+    constructor(message, statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+        this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+        this.isOperational = true;
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+
 // Error handler for async functions
 export const catchAsync = (fn) => {
     return (req, res, next) => {
